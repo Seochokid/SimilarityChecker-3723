642d11a5296412fdb068cfa324cf0b29bed25417
Sunghwan Kim <sung94.kim@samsung.com>
2025-07-02 16:47:58 +0900

    add alphabet similarity

--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -1,9 +1,8 @@
 #include <string>
 #include <algorithm>
+#include <set>
 
-using std::string;
-using std::abs;
-using std::min;
+using namespace std;
 
 class SimilarityChecker {
 public:
@@ -22,5 +21,49 @@ public:
         float minLength = min(length1, length2);
         float diff = abs(length1 - length2);
         return (1.0f - (diff / minLength)) * MAX_SCORE;
+    }
+
+    float checkAlphabetSimilarity(const string& str1, const string& str2) {
+        constexpr float MAX_SCORE = 40.0f;
+        if (!isAlphabetSimilarityValid(str1, str2) || hasLowerCase(str1) || hasLowerCase(str2)) {
+            return 0.0f;
+        }
+        if (str1 == str2) {
+            return MAX_SCORE;
+        }
+        int totalChars = 0, sameChars = 0;
+        getAlphabetStats(str1, str2, totalChars, sameChars);
+
+        return MAX_SCORE * sameChars / totalChars;
+    }
+
+private:
+    bool isAlphabetSimilarityValid(const string& str1, const string& str2) const {
+        return !(str1.empty() || str2.empty());
+    }
+
+    bool hasLowerCase(const string& str) const {
+        for (char c : str) {
+            if (c >= 'a' && c <= 'z') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void getAlphabetStats(const string& str1, const string& str2, int& totalChars, int& sameChars) const {
+        set<char> set1(str1.begin(), str1.end());
+        set<char> set2(str2.begin(), str2.end());
+
+        set<char> totalSet = set1;
+        totalSet.insert(set2.begin(), set2.end());
+
+        totalChars = static_cast<int>(totalSet.size());
+        sameChars = 0;
+        for (char c : set1) {
+            if (set2.count(c)) {
+                ++sameChars;
+            }
+        }
     }
 };
